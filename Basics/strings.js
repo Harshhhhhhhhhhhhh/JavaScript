62f971500b2a72825a4db0e12c3305eec95e07ff
Harsh Raj <126498672+Harshhhhhhhhhhhhh@users.noreply.github.com>
2025-06-26 22:00:23 +0530

    Strings-Methods

---
+++ Basics/strings.js
@@ -0,0 +1,25 @@
+const name = "Harsh";
+const secondName = new String("Jhulla")
+const nickName = new String("     Superstar     ")
+const repoCount = 50;
+
+//console.log(name + repoCount +  "Value");
+
+// console.log(`My name is ${name} and my repoCount is ${repoCount }`);
+// console.log(secondName);
+
+// console.log(secondName[0]);
+// console.log(__proto__);
+// console.log(secondName.length);
+// console.log(secondName.charAt(2));
+// console.log(secondName.indexOf('l'));
+// console.log([secondName.toUpperCase(),secondName]);
+
+//  console.log(name.substring(0,4));
+//  console.log(secondName.slice(-6,4));
+//  console.log(nickName.trim());
+
+ const url = "https://www.youtube.com/watch?v=zVGg-ZqBmZg";
+ console.log(url.replace("?v=","harsh"));
+ console.log(url.includes("harsh"));
+console.log(url.split("/"));
